0e676ac6db0c05c1c38586b249e44a51dc63c408
Matheus Maia Alvarez <88752578+MatheusAlvarez@users.noreply.github.com>
2022-04-14 09:46:06 -0300

    Add files via upload

---
+++ 004/calc.js
@@ -0,0 +1,9 @@
+var somaFunc = require("./soma");
+var subFunc = require("./sub");
+var multFunc = require("./mult");
+var divFunc = require("./div")
+
+console.log(somaFunc(1,2))
+console.log(subFunc(1,2))
+console.log(multFunc(1,2))
+console.log(divFunc(1,2))
