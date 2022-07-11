6951cb881a4559465efe073ce741801a1913b65d
Matheus Maia Alvarez <88752578+MatheusAlvarez@users.noreply.github.com>
2022-07-11 10:03:29 -0300

    Delete 004 directory

--- 004/calc.js
+++
@@ -1,9 +0,0 @@
-var somaFunc = require("./soma");
-var subFunc = require("./sub");
-var multFunc = require("./mult");
-var divFunc = require("./div")
-
-console.log(somaFunc(1,2))
-console.log(subFunc(1,2))
-console.log(multFunc(1,2))
-console.log(divFunc(1,2))
