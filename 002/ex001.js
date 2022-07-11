ae96782438646e406d6d840b225d5799d55be094
Matheus Maia Alvarez <88752578+MatheusAlvarez@users.noreply.github.com>
2022-07-11 10:21:11 -0300

    Upload Ex 002

---
+++ 002/ex001.js
@@ -0,0 +1,10 @@
+const soma = require("./somar.js");
+const sub = require("./sub.js");
+const multi = require("./multi.js")
+const div = require("./div.js")
+
+console.log(soma(5, 5))
+console.log(sub(5, 5))
+console.log(multi(5, 5))
+console.log(div(5, 5))
+
